6666d59bac24868f991fb81dbbdc9576f478c9fc
MKamczyc <mateusz.kamczyc@uczen.zsk.poznan.pl>
2024-05-08 15:14:01 +0200

    fix database relations

--- CatchMe/models.py
+++ CatchMe/models.py
@@ -7,25 +7,37 @@ class user(models.Model):
     lname = models.CharField(max_length=50)
     email = models.EmailField(max_length=60)
     is_admin = models.BooleanField(default = False)
-
-
+    def __str__(self):
+        return self.fname + " "+ self.lname
+    
 
 class team(models.Model):
     id_team = models.AutoField(auto_created = True, primary_key = True)
-    id_leader = models.ForeignKey(user,on_delete = models.CASCADE,related_name = 'leader' )
+    id_leader = models.ForeignKey(user,on_delete = models.CASCADE)
     tname = models.CharField(max_length=100)
-
+    def __str__(self):
+        return self.tname
+    
 
 class project(models.Model):
     id_project = models.AutoField(auto_created = True, primary_key = True)
-    id_team = models.ForeignKey(team,on_delete = models.CASCADE,related_name = 'team' )
+    id_team = models.ForeignKey(team,on_delete = models.CASCADE)
     pname = models.CharField(max_length=50)
     pstart = models.DateField()
-    pend = models.DateField()
+    pend = models.DateField(null = True, blank = True)
     pdeadline = models.DateField()
 
 
 class teamMember(models.Model):
-    id_team = models.ForeignKey(team, on_delete = models.CASCADE,related_name = 'members')
-    id_project = models.ForeignKey(project, on_delete = models.CASCADE,related_name = 'project')
-    id_user = models.ForeignKey(user, on_delete = models.CASCADE,related_name = 'users')
+    id_team = models.ForeignKey(team, on_delete = models.CASCADE)
+    id_project = models.ForeignKey(project, on_delete = models.CASCADE)
+    id_user = models.ForeignKey(user, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return "Nazwa zespołu: "+self.id_team.tname +"| Imie: "+self.id_user.fname +"| Nazwisko: "+self.id_user.lname
+    
+
+class meeting(models.Model):
+    id_project = models.ForeignKey(project, on_delete = models.CASCADE)
+    id_team = id_team = models.ForeignKey(team, on_delete = models.CASCADE)
+    id_leader = id_leader = models.ForeignKey(user,on_delete = models.CASCADE)
