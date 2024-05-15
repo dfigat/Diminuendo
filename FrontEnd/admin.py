ccb3e430f6676bbc736a7009494a6abd8a400b2d
Arystokrates <casimir3thegreat@gmail.com>
2024-05-15 18:41:20 +0200

    Delete FrontEnd

--- FrontEnd/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from CatchMe.models import *
-
-admin.site.register(Meeting)
-admin.site.register(User)
-admin.site.register(Team)
-admin.site.register(TeamMember)
-admin.site.register(Project)
